ec06be931d7dfb41d6f5a95a311f70c630b211ee
ryanb9038 <54568177+ryanb9038@users.noreply.github.com>
2024-03-30 06:07:05 -0500

    No Vehicle Weapon in NoClip TEST

--- vMenu/Noclip.cs
+++ vMenu/Noclip.cs
@@ -85,6 +85,7 @@ namespace vMenuClient
             if (NoclipActive)
             {
                 SetCurrentPedWeapon(Game.PlayerPed.Handle, (uint)GetHashKey("WEAPON_UNARMED"), true);
+                SetVehicleWeaponsDisabled();
                 Scale = RequestScaleformMovie("INSTRUCTIONAL_BUTTONS");
                 while (!HasScaleformMovieLoaded(Scale))
                 {
